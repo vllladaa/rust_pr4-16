d3740a56fd6d7354b68ffbdd2226a219132fe8a6
vlada <krivutsavv@gmail.com>
2024-11-11 21:02:48 +0200

    added code

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    const SIZE: usize = 5;
+
+    for i in 0..SIZE {
+        for _ in 0..(SIZE - i - 1) {
+            print!(" ");
+        }
+        for _ in 0..(2 * i + 1) {
+            print!("*");
+        }
+        println!();
+    }
+
+    for i in (0..SIZE - 1).rev() {
+        for _ in 0..(SIZE - i - 1) {
+            print!(" ");
+        }
+        for _ in 0..(2 * i + 1) {
+            print!("*");
+        }
+        println!();
+    }
 }
